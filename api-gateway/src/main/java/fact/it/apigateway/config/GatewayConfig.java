908ad46be388790ea04800375ed2bbf4e5a79a8c
JannesVdB <r0835818@student.thomasmore.be>
2024-01-04 15:04:48 +0100

    added rate limiting

---
+++ api-gateway/src/main/java/fact/it/apigateway/config/GatewayConfig.java
@@ -0,0 +1,21 @@
+package fact.it.apigateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(5, 10);
+    }
+}
